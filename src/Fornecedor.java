6e9fd04ef6f923e542d40c1ce3a9c069e0827502
João Pinheiro <joaopinheirocavalcante@gmail.com>
2026-06-14 17:01:20 -0300

    Adiciona classes Categoria, Fornecedor e configura o .gitignore

---
+++ src/Fornecedor.java
@@ -0,0 +1,26 @@
+public class Fornecedor {
+    
+    private String nome;
+    private String telefone;
+
+    public Fornecedor(String nome, String telefone) {
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+}
