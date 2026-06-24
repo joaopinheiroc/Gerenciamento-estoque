e48b55df6687601fd288c72ea8cf97ceb4b8fbc2
Alison <jeovaalison00@gmail.com>
2026-06-23 23:28:36 -0300

    adiciona classe Movimentacao

---
+++ src/model/Movimentacao.java
@@ -0,0 +1,18 @@
+public class Movimentacao {
+    
+    private String tipo;
+    private Produto produto;
+    private int quantidade;
+
+    public Movimentacao(String tipo, Produto produto, int quantidade) {
+        this.tipo = tipo;
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public void registrar() {
+        System.out.println("Tipo: " + tipo);
+        System.out.println("Produto: " + produto.getNome());
+        System.out.println("Quantidade: " + quantidade);
+    }
+}
