8d6c89288e3d6b78c39d81fa361785cbcaa85b76
João Pinheiro <joaopinheirocavalcante@gmail.com>
2026-06-24 00:52:56 -0300

    Fix: corrige último commit do EstoqueService

--- src/service/EstoqueService.java
+++ src/service/EstoqueService.java
@@ -1,35 +1,47 @@
-package model;
+package service;
+
 import java.util.ArrayList;
+import java.util.List;
 
-public class Estoque {
+import exceptions.ProdutoNaoEncontradoException;
+import model.Produto;
 
-    private ArrayList<Produto> produtos;
+public class EstoqueService {
 
-    public Estoque() {
-        produtos = new ArrayList<>();
+    private List<Produto> produtos;
+
+    public EstoqueService() {
+        this.produtos = new ArrayList<>();
     }
 
     public void adicionarProduto(Produto produto) {
+        if (produto == null) {
+            throw new IllegalArgumentException("Produto nulo!");
+        }
         produtos.add(produto);
     }
 
-    public void listarProdutos() {
-
-        for (Produto produto : produtos) {
-            produto.exibirDetalhes();
-            System.out.println("---------------------");
-        }
+    public List<Produto> listarProdutos() {
+        return new ArrayList<>(this.produtos);
     }
 
     public Produto buscarProdutoPorId(int id) {
-
         for (Produto produto : produtos) {
-
             if (produto.getId() == id) {
                 return produto;
             }
         }
+        throw new ProdutoNaoEncontradoException("Produto com ID " + id + " não encontrado no estoque.");
+    }
 
-        return null;
+    public void darEntradaEstoque(int id, int quantidade) {
+        Produto produto = buscarProdutoPorId(id);
+        produto.adicionarEstoque(quantidade);
+    }
+
+    public void darSaidaEstoque(int id, int quantidade) {
+        Produto produto = buscarProdutoPorId(id);
+        produto.removerEstoque(quantidade);
     }
 }
+
